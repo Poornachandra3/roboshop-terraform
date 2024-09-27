6fa0a054019edaaa4f040788a406b9814ee44663
Poorna32 <poornachandraga44@gmail.com>
2024-09-27 14:29:30 +0530

    RDS

---
+++ modules/rds/variables.tf
@@ -0,0 +1,13 @@
+variable "component" {}
+variable "env" {}
+variable "family" {}
+variable "instance_class" {}
+variable "allocated_storage" {}
+variable "engine" {}
+variable "engine_version" {}
+variable "skip_final_snapshot" {}
+variable "storage_type" {}
+variable "subnet_ids" {}
+variable "server_app_port_sg_cidr" {}
+variable "vpc_id" {}
+variable "kms_key_id" {}
